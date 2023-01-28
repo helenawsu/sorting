674831861696161ac692e1896bee9b2c43be2842
Helena Su <helenasu@berkeley.edu>
2023-01-28 14:39:37 -0800

    switch order of images with button

--- src/experiment.js
+++ src/experiment.js
@@ -28,7 +28,7 @@ export async function run({ assetPaths, input = {}, environment, title, version 
   const img2 = "<img src = 'assets/2.jpeg'/ height='300'>";
   const img3 = "<img src = 'assets/3.jpeg'/ height='300'>"
   var order = [img1,img2,img3];
-
+  var change = true;
   function orderToHtml(o) {
     var string = "";
     for (var i = 0; i < o.length; i++) {
@@ -46,34 +46,46 @@ export async function run({ assetPaths, input = {}, environment, title, version 
   });
 
   // Welcome screen
-  timeline.push({
-    type: HtmlKeyboardResponsePlugin,
-    stimulus: "<p>Welcome to Sorting!<p/>",
-  });
+  // timeline.push({
+  //   type: HtmlKeyboardResponsePlugin,
+  //   stimulus: "<p>Welcome to Sorting!<p/>",
+  // });
 
+  // var trial1 = {
+  //   type: htmlButtonResponse,
+  //   stimulus: orderToHtml(order),
+  //   choices: ['1', '2', '3', 'exit'],
+  //   prompt: "<p>select any two images to compare, first time</p>"
+  // };
   var trial = {
     type: htmlButtonResponse,
-    stimulus: orderToHtml(order),
-    
+    stimulus: function() {return orderToHtml(order)},
     choices: ['1', '2', '3', 'exit'],
-    prompt: "<p>select any two images to compare</p>"
+    prompt: "<p>select any two images to compare</p>",
+    on_finish: function() {
+      console.log('ORDER IN USE' + order);
+  }
   };
-  
+
+  // timeline.push(trial1);
   var refresh = {
     timeline: [trial],
     prompt: "<p>refresh node</p>",
     conditional_function: function(){
+      console.log("entering refresh node");
       // get the data from the previous trial,
       // and check which key was pressed
       var data1 = jsPsych.data.get().last(1).values()[0];
       var data2 = jsPsych.data.get().last(2).values()[0];
+      if (data2.response != null && change) {
+      // console.log("data2", data2);
       console.log("before change", order);
       var temp = order[data1.response];
       order[data1.response] = order[data2.response];
       order[data2.response] = temp;
-      console.log("temp", temp);
       console.log("after change", order);
-
+      }
+      change = !change;
       if(jsPsych.pluginAPI.compareKeys(String(data1.response), '3')){
           
           return false;
@@ -82,7 +94,7 @@ export async function run({ assetPaths, input = {}, environment, title, version 
   }
   };
   var loopNode = {
-    timeline: [trial, refresh],
+    timeline: [refresh],
     loop_function: function(data){
       var data = jsPsych.data.get().last(1).values()[0];
       if(jsPsych.pluginAPI.compareKeys(String(data.response), '3')){
