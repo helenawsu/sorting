9db03e84a0d39f810dcbb9be3fa0630f47b12ac8
Helena Su <helenasu@berkeley.edu>
2023-01-29 00:01:18 -0800

    switch order only when order is incorrect

--- src/experiment.js
+++ src/experiment.js
@@ -22,19 +22,45 @@ import { initJsPsych } from "jspsych";
  */
 export async function run({ assetPaths, input = {}, environment, title, version }) {
   const jsPsych = initJsPsych();
-
   const timeline = [];
-  const img1 = "<img src = 'assets/1.jpeg'/ height='300'>";
-  const img2 = "<img src = 'assets/2.jpeg'/ height='300'>";
-  const img3 = "<img src = 'assets/3.jpeg'/ height='300'>"
-  var order = [img1,img2,img3];
+
+  // images display in html
+  const img1 = "<img src = 'assets/red.jpeg'/ height='200'>";
+  const img2 = "<img src = 'assets/orange.jpeg'/ height='200'>";
+  const img3 = "<img src = 'assets/yellow.jpeg'/ height='200'>"
+  // the order of images on the screen
+  var displayOrder = [img1,img3,img2];
+  // map/array that maps images to their true order
+  const trueOrderArray = [[img1, 0], [img2, 1], [img3, 2]];
+  const trueOrder = new Map(trueOrderArray);
+  // helper boolean that determines when to swtich image order
   var change = true;
+  
+  // helper method that concatentate displayImg to one single string
   function orderToHtml(o) {
     var string = "";
     for (var i = 0; i < o.length; i++) {
       string += o[i];
     } 
     return string;
+  }
+
+  // return true only if the the selected images are in the wrong order
+  function switchOrNot(imga, imgb) {
+    if (trueOrder.get(imga) > trueOrder.get(imgb)) {
+      var bigger = imga;
+      var smaller = imgb;
+    }
+    else {
+      var bigger = imgb;
+      var smaller = imga;
+    }
+    if (displayOrder.indexOf(bigger) > displayOrder.indexOf(smaller)) {
+      return false;
+    }
+    else{
+      return true
+    }
   }
 
   // Preload assets
@@ -59,31 +85,32 @@ export async function run({ assetPaths, input = {}, environment, title, version 
   // };
   var trial = {
     type: htmlButtonResponse,
-    stimulus: function() {return orderToHtml(order)},
+    stimulus: function() {return orderToHtml(displayOrder)},
     choices: ['1', '2', '3', 'exit'],
     prompt: "<p>select any two images to compare</p>",
     on_finish: function() {
-      console.log('ORDER IN USE' + order);
+      console.log('ORDER IN USE' + displayOrder);
   }
   };
 
   // timeline.push(trial1);
+
+  // determine whether to switch images
   var refresh = {
     timeline: [trial],
     prompt: "<p>refresh node</p>",
     conditional_function: function(){
       console.log("entering refresh node");
-      // get the data from the previous trial,
-      // and check which key was pressed
+      // get the data from the previous two rial, and check which key was pressed (which images are selected)
       var data1 = jsPsych.data.get().last(1).values()[0];
       var data2 = jsPsych.data.get().last(2).values()[0];
-      if (data2.response != null && change) {
+      if (data2.response != null && change && switchOrNot(displayOrder[data1.response], displayOrder[data2.response])) {
       // console.log("data2", data2);
-      console.log("before change", order);
-      var temp = order[data1.response];
-      order[data1.response] = order[data2.response];
-      order[data2.response] = temp;
-      console.log("after change", order);
+      console.log("before change", displayOrder);
+      var temp = displayOrder[data1.response];
+      displayOrder[data1.response] = displayOrder[data2.response];
+      displayOrder[data2.response] = temp;
+      console.log("after change", displayOrder);
       }
       change = !change;
       if(jsPsych.pluginAPI.compareKeys(String(data1.response), '3')){
@@ -93,6 +120,8 @@ export async function run({ assetPaths, input = {}, environment, title, version 
       return true;
   }
   };
+
+  //loops trial
   var loopNode = {
     timeline: [refresh],
     loop_function: function(data){
